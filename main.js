059de05fd38ca967c92ca42395e17ce77382d046
Jose Carlos Junior <josecarlos.jr.pb@gmail.com>
2025-01-19 20:39:44 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,23 @@
+// alert('Hello World');
+
+
+//String
+const message = 'Varial constante';
+
+const firstName = 'Jose Carlos';
+const lastName = 'Junior';
+
+const names = 'Jose, Maria, Pedro, Pablo';
+
+console.log (`Meu nome é ${firstName.toUpperCase()}
+ ${lastName.toLocaleLowerCase()} e sou um programador`);
+
+console.log(names.split(',')); //separar por virgula, caso não coloque nada ele separa por espaços cada letra ou palavra
+
+//.toUpperCase() deixa tudo em maiusculo
+//. toLowerCase() deixa tudo em minusculo
+
+/*let message = 'hello world';
+
+message = 'hola mundo';
+*/
---
+++ string.js
@@ -0,0 +1,7 @@
+const names = ["Jose", "Maria", "Pedro", "Pablo", 10, false];
+
+const Jose = names[0];
+const Maria = names[1]; 
+
+console.log(Jose);
+console.log(Maria);
